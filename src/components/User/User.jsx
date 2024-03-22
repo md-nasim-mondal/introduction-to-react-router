36edfcc9dae1b41e4f823110d3ebf1bb2df3020d
Nasim <nasim.programmer24.com>
2024-03-22 11:39:52 +0600

    we can now find out users details dynamically

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,23 +1,24 @@
-import PropTypes from 'prop-types';
-const User = ({user}) => {
-    const {id, name, email, phone} = user;
-    const userStyle = {
-        border: '2px solid yellow',
-        padding: '5px',
-        borderRadius: '20px'
-    }
-    return (
-        <div style={userStyle}>
-            <h2>Name: {name} </h2>
-            <p>Email: {email} </p>
-            <p>Phone: {phone} </p>
-        </div>
-    );
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+const User = ({ user }) => {
+  const { id, name, email, phone } = user;
+  const userStyle = {
+    border: "2px solid yellow",
+    padding: "5px",
+    borderRadius: "20px",
+  };
+  return (
+    <div style={userStyle}>
+      <h2>Name: {name} </h2>
+      <p>Email: {email} </p>
+      <p>Phone: {phone} </p>
+      <Link to={`/user/${id}`}>Show Details</Link>
+    </div>
+  );
 };
 
-
 User.propTypes = {
-    user: PropTypes.array,
-}
+  user: PropTypes.array,
+};
 
 export default User;
---
+++ src/components/UserDetails/UserDetails.jsx
@@ -0,0 +1,10 @@
+
+const UserDetails = () => {
+    return (
+        <div>
+            <h2>Details About User</h2>
+        </div>
+    );
+};
+
+export default UserDetails;
