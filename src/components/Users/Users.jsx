5dd4cc615e6983c578643b41c16ebf7cab59cb00
Nasim <nasim.programmer24.com>
2024-03-22 11:04:58 +0600

    used children components in router and set outline again

---
+++ src/components/Users/Users.jsx
@@ -0,0 +1,11 @@
+
+const Users = () => {
+    return (
+        <div>
+            <h2>Our Users</h2>
+            <p>Our users are very good persons</p>
+        </div>
+    );
+};
+
+export default Users;
