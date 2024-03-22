787959dcfb1df8fe5f437aeb31d14e20c0ec6ac5
Nasim <nasim.programmer24.com>
2024-03-22 14:58:35 +0600

    learn and used Active route, Loading Spinner, useLocation, useNavigation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,15 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Home = () => {
-    return (
-        <div>
-            <Header></Header>
-            <Outlet></Outlet>
-            <Footer></Footer>
-        </div>
-    );
+  const navigation = useNavigation();
+  const location = useLocation();
+  console.log(location);
+  return (
+    <div>
+      <Header></Header>
+      {navigation.state === "loading" ? (
+        <p>Loading..........</p>
+      ) : (
+        <Outlet></Outlet>
+      )}
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
