bba4b6aa685c8aa74eb81d53798be4e78a835398
Nasim <nasim.programmer24.com>
2024-03-22 10:46:35 +0600

    started working for nested router and used Outlet

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,46 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// eslint-disable-next-line no-unused-vars
 import App from './App.jsx'
 import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Home from './components/Home/Home.jsx';
+import About from './components/About/About.jsx';
+import Contact from './components/Contact/Contact.jsx';
+
+// const router =createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <div>Hello From React Router!</div>,
+//   },
+//   {
+//     path: '/about',
+//     element: <div>I am in the about page</div>
+//   },
+//   {
+//     path: '/contact',
+//     element: <div>Call me right now!!</div>
+//   }
+// ]);
+
 
 const router =createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello From React Router!</div>,
-  },
-  {
-    path: '/about',
-    element: <div>I am in the about page</div>
-  },
-  {
-    path: '/contact',
-    element: <div>Call me right now!!</div>
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path:'/about',
+        element: <About></About>
+      },
+      {
+        path: '/contact',
+        element: <Contact></Contact>
+      }
+    ]
   }
 ]);
 
